4fd3c57a9030925c71c39479a3b776f420fd47c2
bharatg <bharat.ggaikwad@gmail.com>
2020-08-09 05:02:14 +0000

    fixed one.py

--- algorithms/HackerRank/one.py
+++ algorithms/HackerRank/one.py
@@ -1,5 +1,7 @@
-this is not right
-i am not a dictionary
-I am your Brain..
-This is just a random text..
-Don't try to make sense of everything
+# this is not right
+# i am not a dictionary
+# I am your Brain..
+# This is just a random text..
+# Don't try to make sense of everything
+
+import this
